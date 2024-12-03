dff7fb01d45ab7a924735bff071fa063a54f3a68
ShashankaShekharSharma <135298952+ShashankaShekharSharma@users.noreply.github.com>
2024-12-03 23:02:33 +0530

    day end commit

---
+++ FrequencyOfCharacter.java
@@ -0,0 +1,35 @@
+//Calculate the frequency of each character in a string
+import java.util.*;
+class Fequency{
+    private String a;
+    public Fequency(String a){
+        this.a = a;
+    }
+    public void check(){
+        int[] freq = new int[a.length()];
+        char[] string = a.toCharArray();
+        for(int i = 0; i < a.length(); i++){
+            freq[i] = 1;
+            for(int j = i+1; j < a.length(); j++){
+                if(string[i] == string[j]){
+                    freq[i]++;
+                    string[j] = '0';
+                }
+            }
+        }
+        System.out.println("Character Frequency");
+        for(int i = 0; i < freq.length; i++){
+            if(string[i] != ' ' && string[i] != '0'){
+                System.out.println(string[i] + " - " + freq[i]);
+            }
+        }
+    }
+}
+public class FrequencyOfCharacter {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a String: ");
+        String a = sc.nextLine();
+        Fequency frequency = new Fequency(a);
+    }
+}
--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 ## Problems Solved
     - Sum of Digits
     - Pallindrome Check
+    - Largest Number in an Array
